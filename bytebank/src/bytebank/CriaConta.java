5165d019d1e510006258b2b97966b7ef24ffb513
Heitor-Antunes <heitor.dlantunes@gmail.com>
2021-02-15 17:15:08 -0300

    Adicionando a classe  Cria Conta

--- bytebank/src/bytebank/CriaConta.java
+++ bytebank/src/bytebank/CriaConta.java
@@ -2,5 +2,17 @@ package bytebank;
 
 public class CriaConta {
 
-	main
+	public static void main(String[] args) {
+		
+	Conta conta = new Conta();
+	conta.saldo = 200;
+	conta.agencia = 001;
+	conta.numero = 123;
+	conta.titular = "Heitor";
+	
+	System.out.println(conta.saldo);
+	
+
+	}
+
 }
