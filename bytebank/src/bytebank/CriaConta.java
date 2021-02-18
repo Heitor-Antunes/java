bcf5b47f57ff85b4f94025a21ca29071be4ff46c
Heitor-Antunes <heitor.dlantunes@gmail.com>
2021-02-18 10:33:40 -0300

    Adicionando funções a classe Cria Conta

--- bytebank/src/bytebank/CriaConta.java
+++ bytebank/src/bytebank/CriaConta.java
@@ -4,15 +4,27 @@ public class CriaConta {
 
 	public static void main(String[] args) {
 		
-	Conta conta = new Conta();
-	conta.saldo = 200;
-	conta.agencia = 001;
-	conta.numero = 123;
-	conta.titular = "Heitor";
+	Conta primeiraConta = new Conta();
+	primeiraConta.saldo = 200;
+	primeiraConta.agencia = 001;
+	primeiraConta.numero = 123;
+	primeiraConta.titular = "Heitor";
 	
-	System.out.println(conta.saldo);
+	System.out.println(primeiraConta.saldo);
 	
-
+	primeiraConta.saldo += 100;
+	
+	System.out.println("O saldo da Primeira Conta é: " + primeiraConta.saldo);
+	
+	Conta segundaConta = new Conta();
+	segundaConta.saldo = 50;
+	
+	System.out.println("O saldo da Segunda Conta é: " + segundaConta.saldo);
+	
+	System.out.println("A soma dos saldos da Primeira Conta e Segunda Conta é: " + (primeiraConta.saldo + segundaConta.saldo));
+	
+	
+	
 	}
 
 }
