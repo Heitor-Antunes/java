f1e5d082beb74a0312dd773f19702ec52bbaab89
Heitor-Antunes <heitor.dlantunes@gmail.com>
2021-02-23 17:03:00 -0300

    Criando novo projeto Bytebank Composto

---
+++ bytebank-composto/src/bytebank/Conta.java
@@ -0,0 +1,34 @@
+package bytebank;
+
+public class Conta {
+	
+	double saldo;
+	int agencia;
+	int numero;
+	String titular;
+	
+	public void deposita(double valor) {
+		
+		this.saldo += valor;
+	}
+	
+	public boolean saca(double valor) {
+		
+		if(this.saldo >= valor) {
+			this.saldo -= valor;	
+			return true;
+		} else {
+			System.out.println("Saldo insuficiente! Operação não realizada.");
+			return false;
+		}
+	}
+	
+	public boolean transfere(double valor, Conta destino) {
+		
+				if(saca(valor)) {
+				destino.deposita(valor);
+				return true;
+				} return false;
+			}
+
+	}
