84cd76476e56b15c85039c647441f55acf315624
Heitor-Antunes <heitor.dlantunes@gmail.com>
2021-03-05 15:17:00 -0300

    Arrays

--- bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteArrayReferencia.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteArrayReferencia.java
@@ -14,17 +14,31 @@ public class TesteArrayReferencia {
 		ContaPoupanca cp1 = new ContaPoupanca(456, 654);
 		
 //		Conta[] contas = new Conta[3];
-		Conta[] contas = {cc1, cc2, cp1};
+		Object[] contas = {cc1, cc2, cp1};
 		
 		contas[0] = cc1;
 		contas[1] = cc2;
 		contas[2] = cp1;
 		
-		Conta ref = contas[0];
+		//				  Cast
+		Conta ref0 =(ContaCorrente) contas[0];
+//		Conta ref1 = contas[1];
+//		Conta ref2 = contas[2];
 		
-		System.out.println(ref.getNumero());
-		System.out.println(contas[1]);
-		System.out.println(contas[2]);
+		System.out.println(ref0.getNumero());
+//		System.out.println(ref1);
+//		System.out.println(ref2);
+		
+		
+		Object[] obj = {"Heitor", 1, true};
+		
+//		Object ref0 = obj[0];
+		Object ref1 = obj[1];
+		Object ref2 = obj[2];
+		
+		System.out.println(ref0);
+		System.out.println(ref1);
+		System.out.println(ref2);
 
 	}
 
